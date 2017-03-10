cdcb59551a94a14397403c94e11a57d73cc26c3b
Raúl López <rldona@gmail.com>
2017-03-10 09:44:37 +0100

    Component styles changed

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,16 +1,36 @@
 import React, {PropTypes} from 'react';
 
+import Paper from 'material-ui/Paper';
+
 import {Link} from 'react-router';
+
+import FontIcon from 'material-ui/FontIcon';
+import {blue500, red500, greenA200} from 'material-ui/styles/colors';
+
+const iconStyles = {
+  marginRight: 24,
+};
+
 
 class AboutPage extends React.Component {
 
   render() {
+
+    const style = {margin: 20, padding: 20};
+
     return (
-      <div>
+      <Paper style={style} zDepth={1}>
         <h2>About page</h2>
+        <FontIcon
+          className="muidocs-icon-action-home"
+          style={iconStyles}
+          color={red500}
+          hoverColor={greenA200}
+        />
+        <FontIcon className="material-icons" style={iconStyles}>home</FontIcon>
         <p style={{margin: '40px 0'}}>About page descripion</p>
         <Link to="/" className="bnt btn-primary btn-lg">Go to Home page</Link>
-      </div>
+      </Paper>
     );
   }
 
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
+import Paper from 'material-ui/Paper';
+
 import {Link} from 'react-router';
 
 class HomePage extends React.Component {
 
   render() {
+
+    const style = {margin: 20, padding: 20};
+
     return (
-      <div>
+      <Paper style={style} zDepth={1}>
         <h1>React Redux Seed</h1>
         <p style={{margin: '40px 0'}}>React, Redux and ES6</p>
         <Link to="about" className="bnt btn-primary btn-lg">Go to About page</Link>
-      </div>
+      </Paper>
     );
   }
 
